9df04fd4481f2764e36d8c8eea68412c2ad42ce0
july7403 <jsosaorsi@fi.uba.ar>
2024-08-25 18:29:45 -0300

    TP TDA

---
+++ TDA/TP-DyC/desordenado.py
@@ -0,0 +1,2 @@
+#Implementar, por división y conquista, una función que dado un arreglo sin elementos repetidos y casi ordenado (todos los elementos se encuentran ordenados, salvo uno), obtenga el elemento fuera de lugar. Indicar y justificar el orden.
+# Nota sobre RPL: en este ejercicio se pide cumplir la tarea "por división y conquista". Por las características de la herramienta, no podemos verificarlo de forma automática, pero se busca que se implemente con dicha restricción
--- TDA/TP0/tp0-grafos.py
+++ TDA/TP0/tp0-grafos.py
@@ -29,21 +29,18 @@ class Grafo:
         return list(self.adyacencia.keys())
 
 def es_bipartito(grafo):
-    colores = {}  # Diccionario para almacenar los colores de los vértices
+    colores = {}  
 
     def dfs(vertice, color):
-        colores[vertice] = color  # Colorea el vértice actual
+        colores[vertice] = color 
         for vecino in grafo.adyacentes(vertice):
             if vecino not in colores:
-                # Si el vecino no ha sido coloreado, lo coloreamos con el color opuesto
                 if not dfs(vecino, not color):
                     return False
             elif colores[vecino] == colores[vertice]:
-                # Si el vecino ya está coloreado y tiene el mismo color que el vértice actual, no es bipartito
                 return False
         return True
 
-    # Realizamos DFS desde cada vértice no coloreado para manejar grafos desconectados
     for vertice in grafo.vertices():
         if vertice not in colores:
             if not dfs(vertice, True):
@@ -53,13 +50,12 @@ def es_bipartito(grafo):
 
 grafo = Grafo()
 
-# Agregar aristas al grafo
 grafo.agregar_arista(1, 2)
 grafo.agregar_arista(1, 3)
 grafo.agregar_arista(2, 4)
 grafo.agregar_arista(3, 4)
 
-# Verificar si el grafo es bipartito
+
 resultado = es_bipartito(grafo)
 if resultado:
     print("El grafo es bipartito.")
